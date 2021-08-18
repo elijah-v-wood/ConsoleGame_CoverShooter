db1639505de8dcf726e2b536854cef037c9d196c
Elijah Wood <elijah.v.wood@gmail.com>
2021-08-18 13:53:38 -0400

    added grenade consumable

--- ConsoleGame_CoverShooter/Classes/Consumable.cs
+++ ConsoleGame_CoverShooter/Classes/Consumable.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleGame_CoverShooter.Classes
@@ -14,6 +15,29 @@ namespace ConsoleGame_CoverShooter.Classes
         public void UseItem()
         {
             Quantity--;
+        }
+    }
+
+    public class Grenade : Consumable
+    {
+        public void Throw(IShooter enemy)
+        {
+            Random rand = new Random();
+            Thread.Sleep(10);
+            int shot = rand.Next(1, 21);
+            if (shot > enemy.AC)
+            {
+                Thread.Sleep(10);
+                int dmg = rand.Next(1, 7);
+                enemy.HP -= dmg;
+                Console.WriteLine($"you shot the {enemy.GetType().ToString()} for {shot}");
+                Console.WriteLine($"takes {dmg} damage.");
+            }
+            else
+            {
+                Console.WriteLine("You miss.");
+            }
+
         }
     }
 }
