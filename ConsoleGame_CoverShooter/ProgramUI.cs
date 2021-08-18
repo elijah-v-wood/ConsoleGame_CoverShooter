30506fd70198c39b195974c6748f918696cf8272
Elijah Wood <elijah.v.wood@gmail.com>
2021-08-18 11:51:19 -0400

    added ProgramUI and menu functionality

--- ConsoleGame_CoverShooter/ProgramUI.cs
+++ ConsoleGame_CoverShooter/ProgramUI.cs
@@ -6,21 +6,42 @@ using System.Threading.Tasks;
 
 namespace ConsoleGame_CoverShooter
 {
-    public class ProgramUI
+    class ProgramUI
     {
+        bool isRunning = true;
         public void Run()
         {
-            Menu();
+            do
+            {
+                Menu();
+
+            }
+            while (isRunning);
         }
+
 
         public void Menu()
         {
+            Console.WriteLine("Welcome to Cover Shooter!");
+            Console.WriteLine("\n Cover Shooter is a turn-based rogue-like game.");
+            Console.WriteLine("Please select an option below.");
+            Console.WriteLine("1. Start 2. Instructions 3. Exit");
+            string userInput=Console.ReadLine().ToLower();
 
+            switch (userInput)
+            {
+                case "1":
+                case "start":
+                    //Game();
+                    break;
+                case "2":
+                case "instructions":
+                    break;
+                case "3":
+                case "exit":
+                    isRunning = false;
+                    break;
+            }
         }
-
-
-
-
-
     }
 }
