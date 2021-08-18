b0d33fb167d7405515d6e532d35ea212f3d25046
Elijah Wood <elijah.v.wood@gmail.com>
2021-08-18 13:19:48 -0400

    added enemy types

--- ConsoleGame_CoverShooter/Classes/Enemies.cs
+++ ConsoleGame_CoverShooter/Classes/Enemies.cs
@@ -49,7 +49,137 @@ namespace ConsoleGame_CoverShooter.Classes
         {
             TakeCover();
         }
+    }
+    public class RedEnemy:IEnemy, IShooter
+    {
+        public int AC { get; set; } = 10;
+        public int HP { get; set; } = 15;
+        public void TakeCover()
+        {
+            AC = 17;
+            Console.WriteLine("The enemy takes cover!");
+        }
+        public void Shoot(Player player)
+        {
+            AC = 10;
+            Random rand = new Random();
+            Thread.Sleep(10);
+            int shoot = rand.Next(1, 21);
+            if (shoot > player.AC)
+            {
+                Thread.Sleep(10);
+                int dmg = rand.Next(1, 7);
+                player.HP -= dmg;
+                Console.WriteLine($"The enemy shoots you for {shoot}.");
+                Console.WriteLine($"You take {dmg} damage.");
+            }
+            else
+            {
+                Console.WriteLine($"The enemy shoots you for {shoot} and misses.");
+            }
+        }
+        public void Action1(Player player)
+        {
+            Shoot(player);
+        }
 
+        public void Action2(Player player)
+        {
+            Shoot(player);
+        }
+
+        public void Action3(Player player)
+        {
+            Shoot(player);
+        }
+
+    }
+    public class YellowEnemy : IEnemy, IShooter
+    {
+        public int AC { get; set; } = 10;
+        public int HP { get; set; } = 15;
+        public void TakeCover()
+        {
+            AC = 17;
+            Console.WriteLine("The enemy takes cover!");
+        }
+        public void Shoot(Player player)
+        {
+            AC = 10;
+            Random rand = new Random();
+            Thread.Sleep(10);
+            int shoot = rand.Next(1, 21);
+            if (shoot > player.AC)
+            {
+                Thread.Sleep(10);
+                int dmg = rand.Next(1, 7);
+                player.HP -= dmg;
+                Console.WriteLine($"The enemy shoots you for {shoot}.");
+                Console.WriteLine($"You take {dmg} damage.");
+            }
+            else
+            {
+                Console.WriteLine($"The enemy shoots you for {shoot} and misses.");
+            }
+        }
+        public void Action1(Player player)
+        {
+            TakeCover();
+        }
+
+        public void Action2(Player player)
+        {
+            TakeCover();
+        }
+
+        public void Action3(Player player)
+        {
+            TakeCover();
+        }
+
+    }
+    public class GreenEnemy : IEnemy, IShooter
+    {
+        public int AC { get; set; } = 10;
+        public int HP { get; set; } = 15;
+        public void TakeCover()
+        {
+            AC = 17;
+            Console.WriteLine("The enemy takes cover!");
+        }
+        public void Shoot(Player player)
+        {
+            AC = 10;
+            Random rand = new Random();
+            Thread.Sleep(10);
+            int shoot = rand.Next(1, 21);
+            if (shoot > player.AC)
+            {
+                Thread.Sleep(10);
+                int dmg = rand.Next(1, 7);
+                player.HP -= dmg;
+                Console.WriteLine($"The enemy shoots you for {shoot}.");
+                Console.WriteLine($"You take {dmg} damage.");
+            }
+            else
+            {
+                Console.WriteLine($"The enemy shoots you for {shoot} and misses.");
+            }
+        }
+        public void Action1(Player player)
+        {
+            TakeCover();
+        }
+
+        public void Action2(Player player)
+        {
+            Shoot(player);
+        }
+
+        public void Action3(Player player)
+        {
+            Shoot(player);
+        }
 
     }
 }
